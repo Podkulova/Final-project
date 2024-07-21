b284cde13dfdb6d5154c9677e69c574fa6d80db5
Kakika85 <144158769+Kakika85@users.noreply.github.com>
2024-07-21 10:54:00 +0200

    Create ParentRestController

---
+++ src/main/java/org/example/examplefinalProject/controller/restController/ParentRestController.java
@@ -0,0 +1,38 @@
+package org.example.examplefinalProject.controller.restController;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.examplefinalProject.entity.Parent;
+import org.example.examplefinalProject.service.ParentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/api/v1")
+
+public class ParentRestController {
+    private final ParentService parentService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Parent>> index() {
+       return ResponseEntity.ok(parentService.findAll());
+    }
+
+    @GetMapping("/{parentId}")
+    public ResponseEntity<Parent> detail(@PathVariable int parentId){
+        Parent parent = parentService.findById(parentId);
+        if (parent !=null){
+            return ResponseEntity.ok(parent);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
