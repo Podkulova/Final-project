70fefe5a5f9da919bea568b48059e645f3c75fdd
Kakika85 <144158769+Kakika85@users.noreply.github.com>
2024-08-12 10:37:24 +0200

    SCRUM-39-Teacher zakomponování codereview

--- src/main/java/org/example/examplefinalProject/service/ClassRoomService.java
+++ src/main/java/org/example/examplefinalProject/service/ClassRoomService.java
@@ -12,19 +12,36 @@ public class ClassRoomService {
     private final ClassRoomRepository classRoomRepository;
     private final StudentRepository studentRepository;
 
+    public ClassRoom findById(int classRoomId) {
+        return classRoomRepository.findById(classRoomId).orElse(null);
+    }
+
+    /**
+     * Vrací seznam všech tříd.
+     *
+     * @return Seznam tříd.
+     */
     public ClassRoomService(ClassRoomRepository classRoomRepository, StudentRepository studentRepository) {
         this.classRoomRepository = classRoomRepository;
         this.studentRepository = studentRepository;
     }
 
+    /**
+     * Vrací detail třídy.
+     *
+     // @param id Identifikátor třídy.
+     * @return Detail třídy nebo {@code null}.
+     */
     public List<ClassRoom> findAll() {
         return classRoomRepository.findAll();
     }
 
-    public ClassRoom findById(int classRoomId) {
-        return classRoomRepository.findById(classRoomId).orElse(null);
-    }
-
+    /**
+     * Vrací počet studentů v dané třídě.
+     *
+     // @param id Identifikátor třídy.
+     * @return Počet studentů.
+     */
     public int studentCountById(int classRoomId) {
         return studentRepository.countByClassRoomClassRoomId(classRoomId);
     }
